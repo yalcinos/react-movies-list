04c5f797a2b340d6b19ae5599891a4fcd1e70ba6
yalcinos <yalcintatar41@gmail.com>
2020-06-17 15:51:56 -0700

    refactor - Material ui connect with class component

--- src/components/screens/MainScreen/mainScreenStyle.js
+++ src/components/screens/MainScreen/mainScreenStyle.js
@@ -1,10 +1,8 @@
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
+const styles = (theme) => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
   },
-}));
+});
 
-export default useStyles;
+export default styles;
